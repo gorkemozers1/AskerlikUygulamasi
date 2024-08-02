f26f19aa3c9dc8579d1e832578dd359c7354ea58
gorkemozers1 <gorkemozers34@gmail.com>
2024-08-02 16:58:28 +0300

    AskerlikUygulamasi
    Askerlik Uygulaması C#

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Yaşınızı Girin: ");
+int yas = Convert.ToInt32(Console.ReadLine());
+Console.Write("Okuyor Musunuz? (evet: e , hayır: h)");
+string OkuyorMu = Console.ReadLine();
+
+if (OkuyorMu == "h" && yas == >=18){
+    Console.WriteLine("Askere Gelme Yaşınız geldi." );
+}
+else if (OkuyorMu == "e" && yas >=18){
+    Console.WriteLine("Okul Bitince Asker. " );
+}
+
+else {
+    Console.WriteLine("Askerlik Zamanın Gelmedi."); 
+
+}
